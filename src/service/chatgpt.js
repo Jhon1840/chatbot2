d4327e669f38aaee312c783ab84232d6b0591233
jhon1840 <jhonvedia467@gmail.com>
2024-11-25 12:24:20 -0400

    Initial commit after history reset

---
+++ src/service/chatgpt.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getCompletion() {
+    const response = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: "que es lima?",
+      temperature: 0.5,
+      max_tokens: 100,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    });
+    console.log(response.data.choices[0].text);
+}
+
+getCompletion();
